744154a996b4e077ff200860198cf3adc13518e2
Akshit Agarwal <aksh.agg@gmail.com>
2025-11-20 21:37:25 +0530

    chore: remove frontend and past it only in the build

--- frontend/src/pages/auth/index.ts
+++
@@ -1,6 +0,0 @@
-import Login from './Login';
-import Register from './Register';
-import CompleteProfile from './CompleteProfile';
-import AuthSuccess from './AuthSuccess';
-
-export { Login, Register, CompleteProfile, AuthSuccess };
--- frontend/src/pages/property/index.ts
+++
@@ -1,7 +0,0 @@
-import PropertyListing from './PropertyListing';
-import PropertyDetails from './PropertyDetail';
-import PropertyForm from './PropertyForm';
-import CreateProperty from './CreateProperty';
-import EditProperty from './EditProperty';
-
-export { PropertyListing, PropertyDetails, PropertyForm, CreateProperty, EditProperty };
